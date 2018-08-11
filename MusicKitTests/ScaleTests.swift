c2e75c66daee4ff26b68b33fd9d92fd6eb233f2d
Jess Hendricks <jesshmusic@gmail.com>
2018-08-11 09:35:46 -0400

    Fixed some deprecated code in MIDI.swift, updated to Swift 4.1

--- MusicKitTests/ScaleTests.swift
+++ MusicKitTests/ScaleTests.swift
@@ -32,9 +32,21 @@ final class ScaleTests: XCTestCase {
 
     func testChromatic() {
         let sut = Scale.Chromatic(Chroma.c*5)
-        let chromatic: PitchSet = [Chroma.c*5, Chroma.cs*5, Chroma.d*5,
-            Chroma.ds*5, Chroma.e*5, Chroma.f*5, Chroma.fs*5, Chroma.g*5,
-            Chroma.gs*5, Chroma.a*5, Chroma.as*5, Chroma.b*5]
+        let c5 = Chroma.c*5
+        let cs5 = Chroma.cs*5
+        let d5 = Chroma.d*5
+        let ds5 = Chroma.ds*5
+        let e5 = Chroma.e*5
+        let f5 = Chroma.f*5
+        let fs5 = Chroma.fs*5
+        let g5 = Chroma.g*5
+        let gs5 = Chroma.gs*5
+        let a5 = Chroma.a*5
+        let as5 = Chroma.as*5
+        let b5 = Chroma.b*5
+        let chromatic: PitchSet = [c5, cs5, d5,
+            ds5, e5, f5, fs5, g5,
+            gs5, a5, as5, b5]
         XCTAssertEqual(sut, chromatic)
     }
 }
